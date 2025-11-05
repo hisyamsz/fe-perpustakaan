5c5ff63c38741b79b07d83fcac51e869df255bc2
hisyam <hisyam.s901@gmail.com>
2025-11-05 23:05:09 +0700

    init: setup perpustakaan project

---
+++ src/components/commons/PageHead/PageHead.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+interface PropTypes {
+  title?: string;
+}
+
+const PageHead = (props: PropTypes) => {
+  const { title = "Home" } = props;
+
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" href="/images/general/Tixin-icon.png" type="image/x-icon" />
+      <title>{title}</title>
+    </Head>
+  );
+};
+
+export default PageHead;
