6fba64ac4f632edaa51052696cc55424870481dd
hisyam <hisyam.s901@gmail.com>
2025-11-21 23:23:58 +0700

    refactor: change title koleksi buku

--- src/pages/koleksi-buku/index.tsx
+++ src/pages/koleksi-buku/index.tsx
@@ -4,7 +4,7 @@ import KoleksiBuku from "@/components/views/KoleksiBuku";
 export default function KoleksiBukuPage() {
   return (
     <LandingPageLayout
-      title="KoleksiBuku - Perpustakaan SMKN 6 Kota Tangerang Selatan"
+      title="Koleksi Buku - Perpustakaan SMKN 6 Kota Tangerang Selatan"
       description="Jelajahi beragam koleksi buku di Perpustakaan SMKN 6 Kota Tangerang Selatan. Temukan buku berdasarkan judul, kategori, serta buku unggulan yang tersedia untuk mendukung kegiatan belajar dan literasi."
     >
       <KoleksiBuku />
